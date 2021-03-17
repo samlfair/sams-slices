bc169bb7ec2154bbb5acd86e014083f221cf85df
Sam Littlefair <sam.littlefair@prismic.io>
2021-03-17 21:43:56 +0100

    Add squiggly slice

--- slices/EmbedBlock/index.vue
+++ slices/EmbedBlock/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <prismic-embed :field="slice.primary.link" />
+  <!-- <prismic-embed :field="slice.primary.link" /> -->
+  <div v-html="htmlWithoutDimensions" :style="aspectRatioStyle"></div>
 </template>
 
 <script>
 export default {
+  name: "EmbedBlock",
   props: {
     slice: {
       type: Object,
@@ -12,6 +14,31 @@ export default {
         return {};
       }
     }
+  },
+  data() {
+    return {
+      htmlWithoutDimensions: null,
+      aspectRatioStyle: null
+    };
+  },
+  methods: {
+    removeDimensions() {
+      const { html } = this.slice.primary.link;
+      const widthRegex = /width="(...)"/;
+      const heightRegex = /height="(...)"/;
+      const width = html.match(widthRegex)[1];
+      const height = html.match(heightRegex)[1];
+      this.setAspectRatio(width, height);
+      const withoutWidth = html.replace(widthRegex, ``);
+      const withoutHeight = withoutWidth.replace(heightRegex, "");
+      this.htmlWithoutDimensions = withoutHeight;
+    },
+    setAspectRatio(width, height) {
+      this.aspectRatioStyle = { aspectRatio: width / height };
+    }
+  },
+  mounted() {
+    this.removeDimensions();
   }
 };
 </script>
@@ -19,6 +46,6 @@ export default {
 <style scoped>
 ::v-deep iframe {
   width: 100%;
-  height: auto;
+  height: 100%;
 }
 </style>
