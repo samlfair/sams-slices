738018d707950366be16ce8a20a975013a192e11
Sam Littlefair <sam.littlefair@prismic.io>
2021-03-25 17:02:21 +0100

    Order posts in feed

---
+++ slices/index.js
@@ -0,0 +1,8 @@
+export { default as TextBlock } from "./TextBlock";
+export { default as ImageRow } from "./ImageRow";
+export { default as PostFeed } from "./PostFeed";
+
+export { default as EmbedBlock } from './EmbedBlock'
+export { default as SpaceDivider } from './SpaceDivider'
+export { default as SquigglyLine } from './SquigglyLine'
+export { default as GraphicGrid } from './GraphicGrid'
