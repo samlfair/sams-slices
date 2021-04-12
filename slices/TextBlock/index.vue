0ed4ea36d55f193f0cfef73fcd9e5e8ca76fcfc1
Sam Littlefair <sam.littlefair@prismic.io>
2021-04-12 18:23:40 +0200

    Re-add textblock index

---
+++ slices/TextBlock/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="slice-text-block">
+    <prismic-rich-text
+      :htmlSerializer="htmlSerializer"
+      :field="slice.primary.content"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {};
+      }
+    }
+  },
+  methods: {
+    htmlSerializer: function(type, element, content, children) {
+      if (type === "paragraph") {
+        const arrayReplaced = children.map(child => {
+          if (!child.match(/<a.+\/a>/)) {
+            const urlsReplaced = child.replace(
+              /(http:\/\/|https:\/\/)\S+/gi,
+              match => {
+                const regex = /(http:\/\/)?(https:\/\/)?(www)?/i;
+                let anchor = match.replace(regex, "");
+                if (anchor.length > 19) anchor = anchor.slice(0, 19);
+                return `<a href="${match}" target="_blank">${anchor}...</a>`;
+              }
+            );
+            return urlsReplaced;
+          } else {
+            return child;
+          }
+        });
+        return `<p>${arrayReplaced.join("")}</p>`;
+      }
+      return null;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.slice-text-block {
+  display: block;
+}
+</style>
