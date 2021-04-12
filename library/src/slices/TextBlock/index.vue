cb968bf96745596246c1d395c0882f60b9951a3d
Sam Littlefair <sam.littlefair@prismic.io>
2021-04-12 09:06:58 +0200

    Add URL handler to textblock

--- library/src/slices/TextBlock/index.vue
+++ library/src/slices/TextBlock/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="slice-text-block">
-    <prismic-rich-text :field="slice.primary.content" />
+    <prismic-rich-text
+      :htmlSerializer="htmlSerializer"
+      :field="slice.primary.content"
+    />
   </div>
 </template>
 
@@ -13,6 +16,33 @@ export default {
       default() {
         return {};
       }
+    }
+  },
+  methods: {
+    htmlSerializer: function(type, element, content, children) {
+      if (type === "paragraph") {
+        console.log(element);
+        console.log(content);
+        console.log(children);
+        const arrayReplaced = children.map(child => {
+          if (!child.match(/<a.+\/a>/)) {
+            const urlsReplaced = child.replace(
+              /(http:\/\/|https:\/\/)\S+/gi,
+              match => {
+                const regex = /(http:\/\/)?(https:\/\/)?(www)?/i;
+                let anchor = match.replace(regex, "");
+                if (anchor.length > 19) anchor = anchor.slice(0, 19);
+                return `<a href="${match}">${anchor}...</a>`;
+              }
+            );
+            return urlsReplaced;
+          } else {
+            return child;
+          }
+        });
+        return `<p>${arrayReplaced.join("")}</p>`;
+      }
+      return null;
     }
   }
 };
